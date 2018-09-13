bb08b10e256fa42e0b46ddd91b1994754959b927
Diego Roldán <darl.8910@gmail.com>
2018-09-13 17:21:07 -0500

    habilitando el envio del campo google en la busqueda

--- routes/usuario.js
+++ routes/usuario.js
@@ -15,7 +15,7 @@ app.get('/', (request, res, next) => {
     var desde = request.query.desde || 0;
     desde = Number(desde);
 
-    Usuario.find({}, 'nombre email img role')
+    Usuario.find({}, 'nombre email img role google')
         .skip(desde)
         .limit(5)
         .exec((err, usuarios) => {
